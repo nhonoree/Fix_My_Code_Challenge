43a71d0b9afb43b61921d764fef5103b6666ba76
nhonoree <honoreniyirema@gmail.com>
2024-11-23 11:51:55 -0800

    task 0

---
+++ 0x00-challenge/0-fizzbuzz.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def fizzbuzz(n):
+    for i in range(1, n + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz", end=" ")
+        elif i % 3 == 0:
+            print("Fizz", end=" ")
+        elif i % 5 == 0:
+            print("Buzz", end=" ")
+        else:
+            print(i, end=" ")
+
+if __name__ == "__main__":
+    fizzbuzz(50)
